42a0a8d8b12ec2a14dc03744227a26c127e433dd
marten <marten@marten-desktop.(none)>
2011-03-27 13:54:11 +0200

    logger added

---
+++ lib/logconsole.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void logToConsole(char* array) {
+printf("Test");
+	printf("%s\n", array);
+}
---
+++ lib/logfile.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void logInFile(char* logevent) {
+printf("2. Test...\n");
+// FILE* datei;
+
+// sprintf(array, "%s", logevent);
+// datei = fopen("logfile.txt", "a");
+
+ printf("%s\n", logevent);
+
+// fprintf(datei, "%s", logevent);
+// fclose(datei);
+
+}
